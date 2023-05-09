b3f461a518f597185c6e312e74c783e36cd4f3a3
Lane Flores <mflores@star2star.com>
2023-05-08 23:47:10 -0400

    Practice Go - .gitignore

---
+++ larp/larp.go
@@ -0,0 +1,42 @@
+package larp
+
+// A LARP.
+type Game struct {
+	Name         string
+	Description  string
+	Organization Organization
+	System       System
+}
+
+func MakeGame(name string, description string, organization Organization, system System) Game {
+	return Game{
+		Name:         name,
+		Description:  description,
+		Organization: organization,
+		System:       system,
+	}
+}
+
+type Organization struct {
+	Name        string
+	Description string
+}
+
+func MakeOrganization(name string, description string) Organization {
+	return Organization{
+		Name:        name,
+		Description: description,
+	}
+}
+
+type System struct {
+	Name        string
+	Description string
+}
+
+func MakeSystem(name string, description string) System {
+	return System{
+		Name:        name,
+		Description: description,
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"practice-go/larp"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	system := larp.MakeSystem("Dystopia Rising", "The system for DR 3.0.")
+	org := larp.MakeOrganization("DRGA", "The org for DRGA")
+	game := larp.MakeGame("Dystopia Rising Georgia", "The game DRGA.", org, system)
+
+	fmt.Println("The game's name is " + game.Name)
+
 }
