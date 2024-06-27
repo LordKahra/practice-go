134dd63e0934b9c6356a9bee0ef3c8bb3bd244f8
RaiderKing <chaoslfg@gmail.com>
2024-06-27 11:08:55 -0400

    Environment Variables
    moved to env vars

--- src/database/connect.go
+++ src/database/connect.go
@@ -3,17 +3,23 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectToDatabase() (*sql.DB, error) {
-	user := "root"
-	//password := "insecure"
+	user := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	database := os.Getenv("DATABASE_NAME_HAV")
+
+	/*user := "root"
 	password := "insecure"
 	host := "localhost"
 	port := "3306"
-	database := "hav"
+	database := "hav"*/
 
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?parseTime=true&collation=utf8mb4_0900_ai_ci", user, password, host, port, database)
